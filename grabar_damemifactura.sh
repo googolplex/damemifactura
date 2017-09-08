64249d8a361d7eb0e4ea9e3c049abad096d9855a
googolplex <googolplex@lcompras.biz>
2017-09-08 13:49:28 -0400

    pruebo la base firebase

--- grabar_damemifactura.sh
+++ grabar_damemifactura.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-cp *_damemifactura.sh damemifactura/
-rm damemifactura/llave/*
-rm damemifactura/*.*~
-cd damemifactura
+cp ../*_damemifactura.sh damemifactura/
+# borro la llave
+rm ./llave/*
+# borro la basura
+rm /*.*~
+# cd damemifactura
 git add -A
 git commit -a -m "pruebo la base firebase"
 git push -u origin master
 
 # repongo la llave
-cp damemifactura*.json damemifactura/llave/
+cp ../damemifactura*.json damemifactura/llave/
