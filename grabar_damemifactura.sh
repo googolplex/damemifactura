9a5c8696adffab101f4a2438ad83ac8e7f5f1e06
googolplex <googolplex@lcompras.biz>
2017-09-08 13:45:12 -0400

    pruebo la base firebase

--- grabar_damemifactura.sh
+++ grabar_damemifactura.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 cp *_damemifactura.sh damemifactura/
+rm damemifactura/llave/*
 rm damemifactura/*.*~
 cd damemifactura
 git add -A
 git commit -a -m "pruebo la base firebase"
 git push -u origin master
+
+# repongo la llave
+cp damemifactura*.json damemifactura/llave/
---
+++ index.js
@@ -0,0 +1,4 @@
+var firebase = require(['firebase']).initializeApp({
+	serviceAccount: "./llave/damemifactura-5e322b190be6.json",
+	databaseURL: "https://damemifactura-91d4d.firebaseio.com/"
+});
